c4f2b174f26928b62ee531e9d66b39a60dbeea9e
mochluxman <mochlukman786@gmail.com>
2019-11-09 15:41:46 +0700

    Post soal no.2 Fix Validasi Password For Check String Ke 4 dst Huruf Kapital

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
 var username = 'lukman';
-var password = '22&kademy';
+var password = '22&AKAD';
 
 
 function validasi(username) {
@@ -15,7 +15,9 @@ console.log(validasi(username));
 function validasiPass(password) {
     var pass = password.substr(0, 2);
     var pass2 = password.substr(2, 1);
-    if (pass == parseInt(pass) && (pass2 == '@' || pass2 == '&')) {
+    var pass3 = password.substr(3, 4);
+    var pass4 = pass3.toUpperCase();
+    if (pass == parseInt(pass) && (pass2 == '@' || pass2 == '&') && (pass4 == pass3)) {
         return true;
     } else {
         return false;
