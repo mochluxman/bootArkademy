381dbb1b3a119f15f243323cc85a7dc62f643ae4
mochluxman <mochlukman786@gmail.com>
2019-11-09 15:09:36 +0700

    Post soal no.2 Fix Validasi Username Add Validasi Password

--- 2.js
+++ 2.js
@@ -1,11 +1,24 @@
 var username = 'lukman';
-var password = 'arkademy';
+var password = '22@kademy';
 
-function validasi(username, password) {
-    if (username.length >= 5 && username.toLowerCase) {
+
+function validasi(username) {
+    var lun = username.toLowerCase();
+    if (username.length >= 5 && username == lun) {
         return true;
     } else {
         return false;
     }
 }
 console.log(validasi(username));
+
+function validasiPass(password) {
+    var pass = password.substr(0, 2);
+    if (pass == parseInt(pass)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(validasiPass(password));
